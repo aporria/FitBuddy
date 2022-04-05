6ce946308e66d9d96fcc137139dd97ad15cbdac2
aporria <aporria0451@gmail.com>
2022-04-05 16:20:20 -0700

    Added Firebase support

--- lib/home_screens/home.dart
+++ lib/home_screens/home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'new_set.dart';
-import 'existing_set.dart';
+import '../new_set.dart';
+import '../existing_set.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
